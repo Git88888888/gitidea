aaf966f615a4a2093b93c65b5a59084c7f4a62eb
admin <2271140864@qq.com>
2020-04-07 18:40:44 +0800

    idea提交git

---
+++ src/com/itheima/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
